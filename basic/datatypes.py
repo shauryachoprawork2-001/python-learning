53565db9903eb18071cf27cdd9ad53b463a7162b
Shaurya <shauryachoprawork2@gmail.com>
2026-06-16 02:46:06 +0530

    first commit

---
+++ basic/datatypes.py
@@ -0,0 +1,7 @@
+name="shradha"
+age=35
+pi=3.14
+isprime =True
+
+print(type(isprime))
+#boolean
---
+++ basic/firstcode.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+# python3 nameofthefile.py to run the 
+# print is a function 
+# jisko print karaya hai woh string
+
+#\n in the next line
+
+#allowed character 
+# letter a to z
+#digit 0-9
+# operators
+#blanks new line 
+#all ascii &unicode

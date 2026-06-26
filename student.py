16770da2159365670b1eb3afe443d1017e51dadc
Shaurya <shauryachoprawork2@gmail.com>
2026-06-26 05:50:03 +0530

    py

--- student.py
+++ student.py
@@ -1,5 +1,10 @@
 class student:
-    subject="hq"
-    rollno= 23
-stu1=student()
-print(stu1.subject) 
+    def __init__(self,subject,cgpa):
+        self.subject=subject #they are different for different people so they are caled instance attributes 
+        self.cgpa =cgpa
+    def get_cgpa(self):
+        return self.cgpa 
+
+stu1=student("maths",8)
+print(stu1.subject)  
+print(stu1.get_cgpa())
